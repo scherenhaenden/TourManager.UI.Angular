f4abcdf98ff810f7e023ae2fd5941c5d4a2daad4
Eddie Krill <scherenhaenden@gmail.com>
2020-10-30 15:30:03 +0100

    delete unneeded models

--- AngularApp/src/app/models/contacts/contact-model.ts
+++
@@ -1,8 +0,0 @@
-export class ContactModel {
-    public id = 0;
-    public firstName = '';
-    public lastName = '';
-    public telefonNummer = '';
-    public email = '';
-    public address = '';
-}
--- AngularApp/src/app/models/venues/venues-model.ts
+++
@@ -1,9 +0,0 @@
-export class VenuesModels {
-    public id = 0;
-    public name = '';
-    public address = '';
-    public number = '';
-    public contactPersons = '';
-    public loadIn: Date = new Date();
-    public curfView: Date = new Date();
-}
