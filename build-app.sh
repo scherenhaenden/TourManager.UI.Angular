a0598972a5edc3fec01c3a2dae775d39dbcdf853
Eddie Krill <scherenhaenden@gmail.com>
2020-10-30 18:02:59 +0100

    update builder bash

--- build-app.sh
+++ build-app.sh
@@ -2,13 +2,16 @@
 echo 'build starting' 
 echo 'linux build' 
 
-
+cd ./AngularApp/
 npm install
 npm install -g @angular/cli
 
-cd ./AngularApp/
+
 ng build --configuration="production"
 
+cd ..
+mkdir -p ./TourManager.UI.Angular/TourManagerWeb/wwwroot
+cp ./AngularApp/dist/TourManagerUI/* ./TourManager.UI.Angular/TourManagerWeb/wwwroot
 git log --tags --simplify-by-decoration --pretty="format:%ci %d"
 
 
