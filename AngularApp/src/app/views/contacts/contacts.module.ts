c77685e65437a3d25a17326a68933b9f532c9c25
Eddie Krill <scherenhaenden@gmail.com>
2020-11-04 12:24:38 +0100

    Update module to support subrouting

--- AngularApp/src/app/views/contacts/contacts.module.ts
+++ AngularApp/src/app/views/contacts/contacts.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactService } from './../../services/contacts-service';
@@ -6,16 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 import { SummaryComponent } from './summary/summary.component';
+import { ContacsMainComponent } from './contacs-main/contacs-main.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ContactsComponent, SummaryComponent],
+  declarations: [ContactsComponent, SummaryComponent, ContacsMainComponent],
   imports: [
     CommonModule,
-    CommonModule,
     NgbModule,
+    RouterModule,
     SharedComponentsModule,
     FormsModule
   ],
   providers: [ContactService],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class ContactsModule { }
