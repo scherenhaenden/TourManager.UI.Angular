00859729e11fc6702c4b16223fce9ca96cf71b14
Eddie Krill <scherenhaenden@gmail.com>
2020-11-06 16:34:38 +0100

    Update Model

--- AngularApp/src/app/models/venues-model.ts
+++ AngularApp/src/app/models/venues-model.ts
@@ -1,9 +1,14 @@
+import { AddressModel } from './address-model';
+import { TelefonNumberModel } from './telefon-number-model';
+
 export class VenuesModels {
     public id = 0;
     public name = '';
-    public address = '';
-    public number = '';
-    public contactPersons = '';
+    public addresses: AddressModel[] = [];
+    public telefonNumbers: TelefonNumberModel[];
+    public VenuesToContacts: any;
     public loadIn: Date = new Date();
     public curfView: Date = new Date();
+    public MaxCapacity: number;
+    public Notes = '';
 }
