a23e8f8baeee6a615d4820d5db1792ab155d97c4
Eddie Krill <scherenhaenden@gmail.com>
2020-11-13 10:22:17 +0100

    pay tech debs

--- AngularApp/src/app/views/shared-components/formular-components/property-input-formular/property-input-formular.component.ts
+++ AngularApp/src/app/views/shared-components/formular-components/property-input-formular/property-input-formular.component.ts
@@ -1,4 +1,5 @@
 import { ApplicationRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { TimeSpan } from 'src/app/tools/TimeSpan';
 
 @Component({
   selector: 'app-property-input-formular',
@@ -27,9 +28,7 @@ export class PropertyInputFormularComponent implements OnInit {
       this._swapValue = value;
 
 }
-
-
-  // @Input() propertyValue: any = '';
+  
   @Input() type = '';
   @Output() propertyValueEmit: EventEmitter<string> =   new EventEmitter();
   @Output() propertyValueChange: EventEmitter<string> =   new EventEmitter();
@@ -39,7 +38,7 @@ export class PropertyInputFormularComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public updateValueBinding($event: any): void {
+  public updateValueBinding($event: any): void {    
     this.propertyValueChange.emit(this._propertyValue);
   }
 
@@ -55,21 +54,106 @@ export class PropertyInputFormularComponent implements OnInit {
 
       if (this.type === 'time') {
 
+
+        try{
+
+          if(this._propertyValue !== undefined || this._propertyValue !== null) {
+
+            
+
+            if(this._propertyValue instanceof Date) {
+
+              let myV = this._propertyValue as Date;
+
+              this._swapValue = myV.getTime();
+              
+
+
+            }
+            
+            console.log('am i date?', this._swapValue instanceof Date);
+            console.log('am i value?', this._swapValue);
+            
+            
+            let my_val= (this._propertyValue as TimeSpan);
+
+            if(my_val.hours !==0 || my_val.minutes !== 0) {
+
+              let my_val2= (this._propertyValue as Date);
+
+
+              console.log('_propertyValue', this._propertyValue);
+              console.log('_propertyValue ho' , my_val);
+              console.log('_propertyValue hours' , my_val.hours);
+              console.log('_propertyValue seconds' , my_val.hours);
+
+              /*console.log('my_val fixed', my_val);
+              console.log('my_val date', my_val2.getTime());
+              console.log('totalHours fixed', my_val.totalHours);*/
+
+            }
+
+
+            
+          
+
+          //console.log('_propertyValue fixed', this._propertyValue);
+
+          
+
+
+          //console.log('my_val fixed', my_val);
+
+
+          //this._swapValue = my_val.getTime();
+          
+
+          //console.log('my_val fixed gettime', my_val.getTime().toString());
+          
+          //this._propertyValue = this._swapValue;
+
+          //console.log('my_val fixed', my_val.toTimeString());
+
+          //this._propertyValue = this.sCdateToJsDate(this._swapValue);
+
+
+          //this._swapValue = this.sCdateToJsDate(my_val);
+
+
+
+
+          }
+
+          
+        } catch (e) {
+          console.error('error time:', this._swapValue);
+          console.error('error time:', e);
+        }
+
         try {
 
-          console.log('_propertyValue', this._swapValue);
 
-          const final = (this._swapValue as Date).toLocaleTimeString();
+         
+          //console.log('_propertyValue fixed', this._swapValue);
+
+
+
+
+  //        this._propertyValue = this._swapValue;
+//
+          /*const final = (this._swapValue as Date).toLocaleTimeString();
 
           if (this._propertyValue !== (this._swapValue as Date).toLocaleTimeString()) {
 
             this._propertyValue = final;
 
-          }
+          }*/
 
 
         }
-        catch (e) {}
+        catch (e) {
+          console.error('error time:', e)
+        }
 
         return  this.type;
 
