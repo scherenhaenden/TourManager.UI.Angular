6c2fb56c9aae8a856ba6c365f725dac1a1e444b3
Eddie Krill <scherenhaenden@gmail.com>
2020-12-20 17:02:59 +0100

    Draft implementation:
    -Set timer when selected
    -Check how long has been selected
    -if over one second, show extended menu

--- AngularApp/src/app/views/contacts/contacts/contacts.component.ts
+++ AngularApp/src/app/views/contacts/contacts/contacts.component.ts
@@ -13,6 +13,11 @@ export class ContactsComponent implements OnInit {
   public listOfEntites: ContactModel[] = [];
   public rowEntered = null;
 
+  public showExtendedMenu = false;
+  public selectedIrems: ContactModel[] = [];
+
+  private timerSelectionInitiated: number;
+
   constructor(private contactService: ContactService
             , private router: Router) {
 
@@ -28,8 +33,6 @@ export class ContactsComponent implements OnInit {
     const value = await this.contactService.getEntites<ContactModel[]>(50, 1);
     this.listOfEntites = value;
 
-    console.log('myInfo', value);
-
   }
 
   public addNew(): void {
@@ -39,29 +42,52 @@ export class ContactsComponent implements OnInit {
   }
 
 
-  public mouseEnterRow(that: object): void{
+  public mouseEnterRow(rowIndex: number): void{
 
-    this.rowEntered = that;
+    this.rowEntered = rowIndex;
   }
 
-  public mouseLeaveRow(that: object): void {
+  public mouseLeaveRow(rowIndex: number): void {
 
-    if (this.rowEntered === that) {
+    if (this.rowEntered === rowIndex) {
     this.rowEntered = null;
     }
   }
 
 
-  public selectVenewToEdit(item: ContactModel): void {
+  public selectItemStartTimerEitherToEditOrToShowMenu(item: ContactModel, event: Event): void {
 
-    console.log('item', item);
+    this.timerSelectionInitiated = Date.now();
+  }
+
+  public itemMouseRelease(item: ContactModel, event: Event): void{
+
+    const newTime = Date.now();
+
+    if(!this.showExtendedMenu) {
+      const result = newTime - this.timerSelectionInitiated;
+      if(result < 1000){
+        this.selectVenewToEdit(item);
+        return;
+      }
+      this.showDeleteMenu();
+    }
+  
+    this.selectedIrems.push(item);
+
+  }
+
+  public selectVenewToEdit(item: ContactModel): void {
+    
     const itemToEdit = this.listOfEntites
                   .filter(x => x.firstName === item.firstName)[0];
 
     const valueId = itemToEdit.id;
-
     this.router.navigate(['./contacts/summary/' + valueId]);
+  }
 
+  public showDeleteMenu(): void {
+    this.showExtendedMenu = true;
 
   }
 
