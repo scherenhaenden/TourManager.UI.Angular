24391a27af59d2922ea93a59594f8e5eac1fcfd9
scherenhaenden <scherenhaenden@gmail.com>
2020-12-20 20:17:03 +0100

    Development (#28)
    * Delete unneeded files
    
    * Update github action
    
    * Changes:
    
    --Porperties of type array, can be added be clicking on the
    plus button.
    -Delete automatic add of empty entities
    
    * Add EF proxies lib
    
    * Add lazy load configuration
    
    * Add needed properties to models
    
    * Changes:
    
    -Bind delete buttons to its methods
    -Fix repeated id dom for emails
    -Center buttons to deleted, edit and so on
    -Show delete if entity exists
    -dont show add if entity exists
    
    * Add generic httpput method requester
    
    * Create put for contactservice
    
    * Add Delete methods and bind em to the end point action
    
    * Create style for delete button
    
    * Add new version of backend
    
    * Add Frame component for contacs
    
    * updated
    
    * Update routing for contacts
    
    * Update module to support subrouting
    
    * Fix navbar
    
    -make it work with contacts routing
    
    * Delete unneeded component
    
    * Lint
    
    * Create addresses view
    
    -Create addresses view and update models
    to the new version
    
    * Update Model
    
    * Delete unneeded model
    
    * Update model
    
    * Update usings
    
    * Decouple views vor Telefons and Emails
    
    * Update contacts view
    
    * Add title to sub page
    
    * Create Proxy model
    
    -To use exchange data between backend and client the frontend app uses
    Datetime which will be transformed for the api and from the api
    
    * Add model
    
    Add n to m resolver of venues to contact
    
    * pay tech debs
    
    * Delete unneeded code
    
    * Update venue few
    
    * Update model
    
    * Changes
    
    -use unity of work
    -to add a new venues due to the proxy model
    and has to be explicitly casted to venue model
    for adds and updates
    -Lint
    
    * Update engine version
    
    * Lint
    
    * Add TimeSpan
    
    * Decouple views for contacs
    
    * Decouple views for contacts
    
    * Add contacts module
    
    * Export views
    
    * This needs to be changed
    
    * Add contacts view
    
    * Too many updates
    
    * Add decoupled class
    
    * Delete unneeded code
    
    * Delete unneeded code
    
    * Decouple code
    
    * Decouple code
    
    * Changes:
    
    -Remove classes that break the design
    -Add container for all
    
    * Fix
    
    -Fix error for serialization when the object ist reference looped
    
    * Fix updating to parent component
    
    * Update contacts in venue
    
    * Delete unneeded console output
    
    * update module
    
    * Draft pagination for contacts
    
    * Draft update n to m venues contracts
    
    * Update api
    
    * Add pagination in the controller method
    
    * Change request method:
    
    -Now it sends pagination requests
    
    * Changes:
    
    -Get data using pagination
    
    * Type tech deb
    
    * Changes:
    
    -Show cursor as finter when mouse over
    
    * Add Dark mode:
    
    add dark mdoe via less on main styles
    
    * Delete unneeded definition
    
    * Ad builder
    
    * add small read me
    
    * Fix release output
    
    -Create output for app on CI
    - add it to zip

--- build-app.sh
+++ build-app.sh
@@ -14,6 +14,13 @@ mkdir -p ./TourManager.UI.Angular/TourManagerWeb/wwwroot
 cp ./AngularApp/dist/TourManagerUI/* ./TourManager.UI.Angular/TourManagerWeb/wwwroot
 git log --tags --simplify-by-decoration --pretty="format:%ci %d"
 
+echo 'restore solution' 
+cd ./TourManager.UI.Angular/ 
+dotnet restore
+
+echo 'create published solution' 
+dotnet publish -c Release -o out/app/publish -r linux-x64 -p:PublishSingleFile=true --self-contained true
+
 
 
 
